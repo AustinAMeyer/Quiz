7c1236bf4b6f293ad230d4182eb61b812f5a968b
Austin <austin@tag219877.lan1>
2019-06-19 22:33:49 -0500

    Got .xib files working

--- Quiz/AppDelegate.m
+++ Quiz/AppDelegate.m
@@ -7,6 +7,7 @@
 //
 
 #import "AppDelegate.h"
+#import "QuizViewController.h"
 
 @interface AppDelegate ()
 
@@ -17,6 +18,14 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
+    
+    self.window = [[UIWindow alloc] initWithFrame:[[UIScreen mainScreen] bounds]];
+    // Override point for customization after application launch.
+    QuizViewController *test = [[QuizViewController alloc]     initWithNibName:@"QuizViewController" bundle:nil];
+    UINavigationController *nav = [[UINavigationController alloc]  initWithRootViewController:test];
+    self.window.rootViewController = nav;
+    [self.window makeKeyAndVisible];
+    
     return YES;
 }
 
